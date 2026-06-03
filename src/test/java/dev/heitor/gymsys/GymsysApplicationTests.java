6ce00213da1a48b07ddf9b7ff2bb9523ae9d62bd
Heitor-OC <heitorocosta@gmail.com>
2026-06-03 14:43:40 -0300

    first commit

---
+++ src/test/java/dev/heitor/gymsys/GymsysApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.heitor.gymsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
